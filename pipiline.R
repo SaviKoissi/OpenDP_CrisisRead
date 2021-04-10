56aec2db6a03563a701af47a8132ea9eb0a441be
Koissi <70168615+SaviKoissi@users.noreply.github.com>
2021-04-10 21:40:33 +0200

    Add files via upload

--- pipiline.R
+++ pipiline.R
@@ -1,9 +1,9 @@
 # Data aggregation
 require(dplyr)
 require(bigrquery)
-require(styler)
+
 # Make the query from bigquery
-styletext
+
 query <- "select
 activity_day as date,
 concat(from_state_fips, from_county_fips) as from_fips,
@@ -18,10 +18,18 @@ group by(activity_day, from_fips, to_fips) "
 data <- bq_dataset_query(x = "hangar-covid-19.camber_covid_aggregations", query = query) %>%
   bq_table_download(max_results = Inf)
 
-#Overview of the data 
-data.cont<-data %>% group_by(count,activity_day) %>% tally()
-movt.larg<-data.cont %>% filter(n>=10) %>%  print()
+# Overview of the data
+data.cont <- data %>%
+  group_by(count, activity_day) %>%
+  tally()
+movt.larg <- data.cont %>%
+  filter(n >= 10) %>%
+  print()
 
-#  Data winsoring
+# Eliminate improbable movements
+
+#  Data winsorizing
 require(DescTools)
-win.data<-data  %>% mutate(win.var = Winsorize(data$count, minval = tail(Small(data$count, k = 3),1), maxval = head(Large(data$count, k = 3),1))) %>% print() # Here I do not know the reasonable range for a winsorization
+win.data <- data %>%
+  mutate(win.var = Winsorize(data$count, minval = tail(Small(data$count, k = 3), 1), maxval = head(Large(data$count, k = 3), 1))) %>%
+  print() # Here I do not know the reasonable range for a good winsorization
